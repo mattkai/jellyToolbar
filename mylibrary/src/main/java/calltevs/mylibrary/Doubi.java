4842da8e98eee501117d25c2c57bf055ea08626c
kaikai <827221147@qq.com>
2017-06-26 18:45:43 +0800

    代码提交

---
+++ mylibrary/src/main/java/calltevs/mylibrary/Doubi.java
@@ -0,0 +1,12 @@
+package calltevs.mylibrary;
+
+/**
+ * Created by huiyi on 2017/6/26.
+ */
+public class Doubi {
+
+    public static String printText() {
+        return "somebody is doubi?";
+    }
+
+}
